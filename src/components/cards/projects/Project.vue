a5da26f4206108a10a4614e4065700cbc8ace97f
Stefan Novaković <stefan.novakovich@gmail.com>
2017-08-01 17:29:47 +0200

    Break single project into seperate component

---
+++ src/components/cards/projects/Project.vue
@@ -0,0 +1,141 @@
+<template>
+  <section>
+    <div class="title">
+      <h3>{{ project.title }}</h3>
+      <p>{{ project.summary }}</p>
+    </div>
+    <div class="image-wrapper">
+      <div class="inner">
+        <image-slider :images="getImages(project)"></image-slider>
+      </div>
+    </div>
+    <div class="info">
+      <p v-html="project.description"></p>
+      <div class="breakdown">
+        <p>
+          <span class="group">Period:</span>
+          <span>{{ formatPeriod(project.period) }}</span>
+        </p>
+        <p v-if="project.client">
+          <span class="group">Client:</span>
+          <a v-if="project.client.link" :href="project.client.link">{{ project.client.name }}</a>
+          <span v-else>{{ project.client.name }}</span>
+        </p>
+        <p v-if="project.duties">
+          <span class="group">Duties:</span><span>{{ project.duties }}</span>
+        </p>
+        <p v-if="project.link">
+          <span class="group">Link:</span>
+          <a href="project.link">{{ project.link }}</a>
+        </p>
+        <p class="type">
+          <span class="group">Type:</span>
+          <span class="tag" v-for="type in project.type" :key="type"
+            :class="{ active: type === typeFilter }"
+            title="Click to apply this filter"
+            @click="setTypeFilter(type)">{{ type }}</span>
+        </p>
+        <p>
+          <span class="group">Technologies:</span>
+          <span class="tag" v-for="technology in project.technologies" :key="technology"
+            :class="{ active: technology === technologyFilter }"
+            title="Click to apply this filter"
+            @click="setTechnologyFilter(technology)">{{ technology }}</span>
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<style lang="scss" scoped>
+  @import '~styles/constants';
+
+  .title {
+    text-align: center;
+    margin-bottom: 20px;
+
+    h3 { margin-bottom: 0; }
+
+    p {
+      color: $light-txt-color;
+      margin-top: 0px;
+    }
+  }
+
+  .image-wrapper {
+    background: $bg-color;
+    padding: 5px;
+    border: 6px double $dark-bg-color;
+
+    .inner {
+      min-height: 250px;
+    }
+  }
+
+  .info {
+    padding: 10px 30px 20px 30px;
+
+    p { color: $txt-color; }
+    .tag { cursor: pointer; }
+
+    .breakdown {
+      text-align: left;
+      p { margin: 7.5px 0; }
+      p.type { margin: 15px 0 10px 0; }
+    }
+
+    .group {
+      text-transform: uppercase;
+      font-weight: bold;
+      color: $header-color;
+      font-size: 13px;
+      margin-right: 5px;
+      display: inline-block;
+      min-width: 52px;
+    }
+  }
+</style>
+
+
+<script>
+  import months from 'common/months';
+
+  import ImageSlider from '../../ImageSlider.vue';
+
+  export default {
+    props: { project: Object },
+    computed: {
+      typeFilter() { return this.$store.state.projects.filter.type; },
+      technologyFilter() { return this.$store.state.projects.filter.technology; }
+    },
+    methods: {
+      getImages(project) {
+        const images = [];
+        if (project.images && project.images.total) {
+          const extension = project.images.extension || 'jpg';
+          for (let i = 1; i <= project.images.total; i++) { // eslint-disable-line no-plusplus
+            images.push(`assets/images/projects/${project.images.folder}/${i}.${extension}`);
+          }
+        }
+        return images;
+      },
+      formatPeriod(period) {
+        const start = `${months[period.start.getMonth()]} ${period.start.getFullYear()}`;
+        if (!period.end) {
+          return `${start} - Present`;
+        }
+        return `${start} - ${months[period.end.getMonth()]} ${period.end.getFullYear()}`;
+      },
+      setTypeFilter(type) {
+        this.$store.commit('setTypeFilter', type);
+      },
+      setTechnologyFilter(technology) {
+        this.$store.commit('setTechnologyFilter', technology);
+      }
+    },
+    components: {
+      ImageSlider
+    }
+  };
+</script>
--- src/components/cards/projects/index.vue
+++ src/components/cards/projects/index.vue
@@ -2,50 +2,7 @@
   <card id="projects-card" class="projects">
     <h2>Projects</h2>
     <div class="item" v-for="project in activeProjects" :key="project.title">
-      <div class="title">
-        <h3>{{ project.title }}</h3>
-        <p>{{ project.summary }}</p>
-      </div>
-      <div class="image-wrapper">
-        <div class="inner">
-          <image-slider :images="getImages(project)"></image-slider>
-        </div>
-      </div>
-      <div class="info">
-        <p v-html="project.description"></p>
-        <div class="breakdown">
-          <p>
-            <span class="group">Period:</span>
-            <span>{{ formatPeriod(project.period) }}</span>
-          </p>
-          <p v-if="project.client">
-            <span class="group">Client:</span>
-            <a v-if="project.client.link" :href="project.client.link">{{ project.client.name }}</a>
-            <span v-else>{{ project.client.name }}</span>
-          </p>
-          <p v-if="project.duties">
-            <span class="group">Duties:</span><span>{{ project.duties }}</span>
-          </p>
-          <p v-if="project.link">
-            <span class="group">Link:</span>
-            <a href="project.link">{{ project.link }}</a>
-          </p>
-          <p class="type">
-            <span class="group">Type:</span>
-            <span class="tag" v-for="type in project.type" :key="type"
-              :class="{ active: type === typeFilter }"
-              title="Click to apply this filter"
-              @click="setTypeFilter(type)">{{ type }}</span>
-          </p>
-          <p>
-            <span class="group">Technologies:</span>
-            <span class="tag" v-for="technology in project.technologies" :key="technology"
-              :class="{ active: technology === technologyFilter }"
-              title="Click to apply this filter"
-              @click="setTechnologyFilter(technology)">{{ technology }}</span>
-          </p>
-        </div>
-      </div>
+      <project :project="project"></project>
     </div>
     <div class="pagination">
       <pagination :items="projects" @change="setActive"></pagination>
@@ -64,58 +21,6 @@
     border-bottom: 1px solid $bg-color;
   }
 
-  .title {
-    text-align: center;
-    margin-bottom: 20px;
-
-    h3 {
-      margin-bottom: 0;
-    }
-
-    p {
-      color: $light-txt-color;
-      margin-top: 0px;
-    }
-  }
-
-  .image-wrapper {
-    background: $bg-color;
-    padding: 5px;
-    border: 6px double $dark-bg-color;
-
-    .inner {
-      min-height: 250px;
-    }
-  }
-
-  .info {
-    padding: 10px 30px 20px 30px;
-
-    p {
-      color: $txt-color;
-    }
-
-    .tag {
-      cursor: pointer;
-    }
-
-    .breakdown {
-      text-align: left;
-      p { margin: 7.5px 0; }
-      p.type { margin: 15px 0 10px 0; }
-    }
-
-    .group {
-      text-transform: uppercase;
-      font-weight: bold;
-      color: $header-color;
-      font-size: 13px;
-      margin-right: 5px;
-      display: inline-block;
-      min-width: 52px;
-    }
-  }
-
   .pagination {
     position: relative;
 
@@ -129,21 +34,18 @@
 
 
 <script>
-  import months from 'common/months';
   import projects from 'common/projects';
   import scrollTo from 'common/scrollTo';
 
   import Card from '../Card.vue';
   import FilterTooltip from './FilterTooltip.vue';
   import FilteredBy from './FilteredBy.vue';
-  import ImageSlider from '../../ImageSlider.vue';
   import Pagination from '../../Pagination.vue';
+  import Project from './Project.vue';
 
   export default {
     data() {
-      return {
-        activeProjects: []
-      };
+      return { activeProjects: [] };
     },
     computed: {
       typeFilter() { return this.$store.state.projects.filter.type; },
@@ -156,42 +58,19 @@
       }
     },
     methods: {
-      getImages(project) {
-        const images = [];
-        if (project.images && project.images.total) {
-          const extension = project.images.extension || 'jpg';
-          for (let i = 1; i <= project.images.total; i++) { // eslint-disable-line no-plusplus
-            images.push(`assets/images/projects/${project.images.folder}/${i}.${extension}`);
-          }
-        }
-        return images;
-      },
       setActive(items) {
         this.activeProjects = items;
         if (this.$el) {
           scrollTo(this.$el);
         }
-      },
-      formatPeriod(period) {
-        const start = `${months[period.start.getMonth()]} ${period.start.getFullYear()}`;
-        if (!period.end) {
-          return `${start} - Present`;
-        }
-        return `${start} - ${months[period.end.getMonth()]} ${period.end.getFullYear()}`;
-      },
-      setTypeFilter(type) {
-        this.$store.commit('setTypeFilter', type);
-      },
-      setTechnologyFilter(technology) {
-        this.$store.commit('setTechnologyFilter', technology);
       }
     },
     components: {
       Card,
       FilterTooltip,
       FilteredBy,
-      ImageSlider,
-      Pagination
+      Pagination,
+      Project
     }
   };
 </script>
