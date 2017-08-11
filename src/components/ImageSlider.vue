576ac6d86f5f71f643e825a65de6c56cd0193ee5
Stefan Novaković <stefan.novakovich@gmail.com>
2017-08-11 09:55:07 +0200

    Image slider: update on resize

--- src/components/ImageSlider.vue
+++ src/components/ImageSlider.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="sn-slider">
-    <slider v-if="images.length" height="350px"
+    <slider v-if="images.length"
+      height="350px"
+      ref="slider"
       :auto="false"
       :indicators="images.length > 1 ? 'center': false"
       :control-btn="images.length > 1">
@@ -48,17 +50,23 @@
   @import '~styles/constants';
 
   .sn-slider {
-    .slider-btn {
-      background: none;
+    .btn {
+      background: none!important;
       opacity: 1;
       margin-top: -5px;
+
+      &:hover {
+        .slider-icon {
+          border-color: $brand-color;
+        }
+      }
     }
 
     .slider-icon {
+      transition: .5s border-color;
       border-left: 3px solid $alt-brand-color;
       border-bottom: 3px solid $alt-brand-color;
     }
-
 
     .slider-indicator-icon {
       background: rgba(33, 33, 33, 0.4);
@@ -80,6 +88,16 @@
     },
     data() {
       return { list: null };
+    },
+    methods: {
+      reRenderSlider() {
+        if (this.$refs.slider) {
+          this.$refs.slider.$emit('$VueEasySlider:reRender');
+        }
+      }
+    },
+    mounted() {
+      window.addEventListener('resize', this.reRenderSlider);
     },
     components: {
       Slider,
