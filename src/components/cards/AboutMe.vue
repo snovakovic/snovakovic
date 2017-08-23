43fd5d9c23dd0b81294cad2fbaecfe71b45178dd
Stefan Novaković <stefan.novakovich@gmail.com>
2017-08-23 11:16:25 +0200

    Update about me section

--- src/components/cards/AboutMe.vue
+++ src/components/cards/AboutMe.vue
@@ -4,16 +4,22 @@
     <blockquote class="tldr">
       <b>TL;DR - </b> JavaScript enthusiast that writes clean, readable and modular code.
     </blockquote>
+    <p></p>
     <p>
       I'm Professional software developer with a Master’s degree in Computer Science and
-      more than {{ yearOfExperiance }} years of industry experience working on multiple projects from
-      small one person ventures to large 15+ person projects. Agile, direct
-      communication with internal stakeholders, external clients, code reviews and
+      more than {{ yearOfExperiance }} years of industry experience.
+      Agile, direct communication with internal stakeholders, external clients, code reviews and
       unit testing. I have in-depth experience across the entire development cycle.
     </p>
     <p>
       I'm <b>passionate</b> about <b>Full Stack JavaScript</b> development.
       Beside JavaScript I have lot of experiance in C# (.NET) but in recent years that part has been negliected in favor of JavaScript.
+    </p>
+    <p>
+      <b>Interesting in seing my work in action?</b> Take a peek at my <a href="https://github.com/snovakovic">github</a>
+      account, my open source projects on <a href="https://www.npmjs.com/~snovakovic">npm</a>
+      or at any of my contributions to Open Source projects which become my passion in recent time.
+      I belive it's time to give back to community after all these years of using greate open source projects in every day development.
     </p>
   </card>
 </template>
