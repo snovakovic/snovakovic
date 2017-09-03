cc90c4dde82e8387eb1dc12fadc527dca221af6b
snovakovic <stefan.novakovich@gmail.com>
2017-09-03 20:43:28 +0200

    Add high school to Education

--- src/components/cards/Education.vue
+++ src/components/cards/Education.vue
@@ -3,9 +3,9 @@
     <h2><i class="fa fa-graduation-cap"></i> Education</h2>
     <div class="item" v-for="study in education" :key="study.title">
       <h4 class="title">{{ study.title }}</h4>
-      <p class="university">
-        <a class="dark" :href="study.university.link">{{ study.university.name }}</a>
-        <span class="year">({{ study.period }})</span>
+      <p class="institution">
+        <a class="dark" :href="study.institution.link">{{ study.institution.name }}</a>
+        <span class="year">{{ study.period }}</span>
       </p>
     </div>
   </card>
@@ -27,7 +27,7 @@
     margin-right: 2px;
   }
 
-  .university {
+  .institution {
     font-size: 13px;
     font-weight: bold;
     margin-top: 5px;
@@ -45,25 +45,31 @@
 <script>
   import Card from './Card.vue';
 
-  const university = {
+  const FESB = {
     name: 'Technical University of Split',
     link: 'https://eng.fesb.unist.hr/'
   };
 
-  const education = [{
-    title: 'MSc Computer Science',
-    period: '2009-2011',
-    university
-  }, {
-    title: 'BSc Computer Science',
-    period: '2006-2009',
-    university
-  }];
-
-
   export default {
     data() {
-      return { education };
+      return {
+        education: [{
+          title: 'MSc Computer Science',
+          period: '2009-2011',
+          institution: FESB
+        }, {
+          title: 'BSc Computer Science',
+          period: '2006-2009',
+          institution: FESB
+        }, {
+          title: 'Computer Tehnician for Engineering',
+          period: '2002-2006',
+          institution: {
+            name: 'Technical High School of Split',
+            link: 'ss-tehnicka-st.skole.hr'
+          }
+        }]
+      };
     },
     components: {
       Card
