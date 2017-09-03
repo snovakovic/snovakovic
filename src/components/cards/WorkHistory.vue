879099eb1bfce47eaec9ff911586208905777f8d
snovakovic <stefan.novakovich@gmail.com>
2017-09-03 16:51:11 +0200

    Move work history to data file

--- src/components/cards/WorkHistory.vue
+++ src/components/cards/WorkHistory.vue
@@ -1,46 +1,17 @@
 <template>
   <card class="work-history">
     <h2>Work History</h2>
-    <div class="item">
+    <div class="item" v-for="item in workHistory" :key="item.period">
       <h3 class="title">
-        Sr. Software Developer <i class="fa fa-circle"></i>
-        <span class="company"><a href="https://extensionengine.com/">ExtensionEngine</a></span>
-        <span class="year">Aug 2014 - Present</span>
+        {{ item.position }}
+        <i class="fa fa-circle"></i>
+        <span class="company">
+          <a v-if="item.company.link" :href="item.link">{{ item.company.name }}</a>
+          <span v-else>{{ item.company.name }}</span>
+        </span>
+        <span class="year">{{ item.period }}</span>
       </h3>
-      <p>
-        Back to roots at ExtensionEngine but this time in new role as Senior Software Developer.
-        Now I'm no longer the one who is learning I'm the one who is passing knowlage,
-        expiriance and mentoring to junior team members.Swithced my focus from
-        Full Stack C#(.NET) development to Full Stack JavaScript development which become
-        my main focus and passion.Worked on multiple interesting international projects with main
-        focus on projects for Ellevation Education,QSF and the current project for University of NotreDame. Take a look at projects sections for more information on them.
-      </p>
-    </div>
-    <div class="item">
-      <h3 class="title">
-        Software Developer <i class="fa fa-circle"></i>
-        <span class="company"><a href="http://www.king-ict.hr/">King ICT</a></span>
-        <span class="year">Jan 2014 - Aug 2014</span>
-      </h3>
-      <p>
-        Relocated to Zagreb to work for one of bigest Croatian IT companies.
-        They main focuse are big projects for goverment.
-        My whole time there I was working on new business application for Zagreb Municipal Transit System (ZET).
-      </p>
-    </div>
-    <div class="item">
-      <h3 class="title">
-        Jr. Software Developer <i class="fa fa-circle"></i>
-        <span class="company"><a href="https://extensionengine.com/">ExtensionEngine</a></span>
-        <span class="year">Sept 2011 – Jan 2014</span>
-      </h3>
-      <p>
-        Fresh from collage thrown in fire of large international projects,
-        learned a lot in first 2 years of working for ExtensionEngine.
-        Mostly worked with JavaScript and C# programming languages.
-        Most important projects form this period are Frontier Strategy Group and PHT.
-        Take a look at projects section for more information on those projects.
-      </p>
+      <p>{{ item.summary }}</p>
     </div>
   </card>
 </template>
@@ -76,9 +47,15 @@
 
 
 <script>
+  import WorkHistory from 'data/WorkHistory';
   import Card from './Card.vue';
 
   export default {
+    data() {
+      return {
+        workHistory: WorkHistory
+      };
+    },
     components: {
       Card
     }
---
+++ src/data/WorkHistory.js
@@ -0,0 +1,36 @@
+const extensionEngine = {
+  name: 'ExtensionEngine',
+  link: 'https://extensionengine.com/'
+};
+
+export default [{
+  position: 'Sr. Software Developer',
+  period: 'Aug 2014 - Present',
+  company: extensionEngine,
+  summary: `Back to roots at ExtensionEngine but this time in new role as Senior Software Developer.
+    Now I'm no longer the one who is learning I'm the one who is passing knowlage,
+    expiriance and mentoring to junior team members.Swithced my focus from
+    Full Stack C#(.NET) development to Full Stack JavaScript development which become
+    my main focus and passion.Worked on multiple interesting international projects with main
+    focus on projects for Ellevation Education,QSF and the current project for University of NotreDame.
+    Take a look at projects sections for more information on them.`
+}, {
+  position: 'Software Developer',
+  period: 'Jan 2014 - Aug 2014',
+  company: {
+    name: 'King ICT',
+    link: 'http://www.king-ict.hr/'
+  },
+  summary: `Relocated to Zagreb to work for one of bigest Croatian IT companies.
+    They main focuse are big projects for goverment.
+    My whole time there I was working on new business application for Zagreb Municipal Transit System (ZET).`
+}, {
+  position: 'Jr. Software Developer',
+  period: 'Sept 2011 – Jan 2014',
+  company: extensionEngine,
+  summary: `Fresh from collage thrown in fire of large international projects,
+    learned a lot in first 2 years of working for ExtensionEngine.
+    Mostly worked with JavaScript and C# programming languages.
+    Most important projects form this period are Frontier Strategy Group and PHT.
+    Take a look at projects section for more information on those projects.`
+}];
