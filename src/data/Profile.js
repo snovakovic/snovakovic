1154f6c1d2e70e913165374a5487786be1fd5d3e
Stefan Novaković <stefan.novakovich@gmail.com>
2017-09-05 13:44:02 +0200

    Change stackoverflow to point to story link

--- src/data/Profile.js
+++ src/data/Profile.js
@@ -4,6 +4,6 @@ export default {
   linkedin: 'https://www.linkedin.com/in/snovakovic87',
   npm: 'https://www.npmjs.com/~snovakovic',
   resume: 'https://drive.google.com/file/d/0B5nqK8_IZiBBWGZINl9hb012Nk0/view?usp=sharing',
-  stackoverflow: 'https://stackoverflow.com/users/1804295/snovakovic?tab=profile',
+  stackoverflow: 'https://stackoverflow.com/story/snovakovic',
   upwork: 'https://www.upwork.com/o/profiles/users/_~017c648c2aba395f29/'
 };
